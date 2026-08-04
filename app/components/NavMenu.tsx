4f75f71146932677f7cc27eb7f15662b1b9f2c02
Mi-Yeon-Kim <228261317+Mi-Yeon-Kim@users.noreply.github.com>
2026-08-03 20:45:40 -0400

    update NavMenu.tsx fix typo

--- app/components/NavMenu.tsx
+++ app/components/NavMenu.tsx
@@ -10,7 +10,7 @@ export default function NavMenu()
             <ul className="flex flex-row gap-4">
                 <li><a href="#about">About</a></li>
                 <li><a href="#experience">Experience</a></li>
-                <li><a href="#proejcts">Projects</a></li>
+                <li><a href="#projects">Projects</a></li>
             </ul>
         </span>
     </nav>
