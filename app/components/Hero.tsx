f89fccf1cca19eafb16ac1f470b2da723f762545
Mi-Yeon-Kim <228261317+Mi-Yeon-Kim@users.noreply.github.com>
2026-08-03 20:53:19 -0400

    fix issue with z-index of absolute bg elements

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -5,9 +5,9 @@ export default function Hero({content} : {content: string})
 {
     return (
     <section className="relative w-full h-screen flex items-center justify-center">
-        <div className="absolute right-15 -top-30 bg-primary-400/10 rounded-full size-200 blur-[100px]"></div>
-        <div className="absolute -left-40 top-130 bg-blue-400/5 rounded-full size-200 blur-[100px]"></div>
-        <div className="absolute w-full h-[230vh] bg-linear-to-b/oklch from-primary-400/20 to-primary-400/0"></div>
+        <div className="absolute -z-50 right-15 -top-30 bg-primary-400/10 rounded-full size-200 blur-[100px]"></div>
+        <div className="absolute -z-50 -left-40 top-130 bg-blue-400/5 rounded-full size-200 blur-[100px]"></div>
+        <div className="absolute -z-50 w-full h-[230vh] bg-linear-to-b/oklch from-primary-400/20 to-primary-400/0"></div>
 
         <div className="container text-center flex flex-col gap-8">
             <p className="text-9xl font-medium">I am {profile.person.firstName}.</p>
