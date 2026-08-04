f94fc93564421b7de18e9d44d415cc44eaf37d84
Mi-Yeon-Kim <228261317+Mi-Yeon-Kim@users.noreply.github.com>
2026-08-03 21:20:01 -0400

    add animations to About section

--- app/components/AboutContent.tsx
+++ app/components/AboutContent.tsx
@@ -1,5 +1,39 @@
 import { Profile } from "../Types/Profile";
+import * as motion from "motion/react-client";
+import { Variants, ViewportOptions } from "motion";
 
 export default function AboutContent({content}: {content: Profile['about']}) {
-    return <p className="inline-1/3 text-2xl text-gray-400 font-light">{content.detail}</p>;
+    const slideFadeInRightToLeft: Variants = {
+        hidden:
+        {
+            opacity: 0,
+            x: 100
+        },
+        visible:
+        {
+            opacity: 1,
+            x: 0,
+            transition:
+            {
+                duration: 0.4,
+                ease: "easeOut"
+            }
+        }
+    };
+
+    const viewportOptions: ViewportOptions = {
+        once: true,
+        amount: 0.4
+    };
+
+
+    return (
+        <motion.p 
+                    initial="hidden"
+            whileInView="visible"
+            variants={slideFadeInRightToLeft}
+            viewport={viewportOptions}
+            className="inline-1/3 text-2xl text-gray-400 font-light"
+        >{content.detail}</motion.p>
+    );
 }
--- app/components/ProfileAside.tsx
+++ app/components/ProfileAside.tsx
@@ -1,17 +1,48 @@
 import Avatar from "./Avatar";
 import ProfileLinks from "./ProfileLinks";
 import { Profile } from "../Types/Profile";
-
+import * as motion from "motion/react-client";
+import { Variants, ViewportOptions } from "motion";
 
 export default function ProfileAside({person, links}: { person: Profile["person"], links: Profile["links"] }) {
+
+    const slideFadeInLeftToRight: Variants = {
+        hidden:
+        {
+            opacity: 0,
+            x: -100
+        },
+        visible:
+        {
+            opacity: 1,
+            x: 0,
+            transition:
+            {
+                duration: 0.4,
+                ease: "easeOut"
+            }
+        }
+    };
+
+    const viewportOptions: ViewportOptions = {
+        once: true,
+        amount: 0.4
+    };
+
     return (
-        <aside className="inline-1/3 flex flex-row items-start gap-5">
+        <motion.aside 
+            initial="hidden"
+            whileInView="visible"
+            variants={slideFadeInLeftToRight}
+            viewport={viewportOptions}
+            className="inline-1/3 flex flex-row items-start gap-5"
+        >
             <Avatar />
             <div className="grow ml-5 text-5xl font-semibold flex flex-col h-[200px] justify-between">
                 <span className="grow-2">{person.firstName}</span>
                 <span className="grow-2">{person.lastName}</span>
                 <span className="grow-2 content-end"><ProfileLinks links={links}/></span>
             </div>
-        </aside>
+        </motion.aside>
     );
 }
