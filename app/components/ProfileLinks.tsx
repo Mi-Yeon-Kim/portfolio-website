3a3a50082e42f1ac073da8b93c85e4e447ab2ec0
Mi-Yeon-Kim <228261317+Mi-Yeon-Kim@users.noreply.github.com>
2026-08-06 23:16:28 -0400

    Update about section color scheme

--- app/components/ProfileLinks.tsx
+++ app/components/ProfileLinks.tsx
@@ -1,12 +1,13 @@
 import { ReactNode } from "react";
 import { Profile } from "../Types/Profile";
+import { FaGithub, FaLinkedinIn, FaEnvelope, FaFileDownload } from "react-icons/fa";
 
 
 export function ProfileLink({children, profile}: {children: ReactNode, profile: string})
 {
     return (
-        <li className="size-15 sm:size-10 rounded-full border-4 sm:border border-primary-400 inline-block text-center inline-grid place-items-center">
-            <a href={profile} >
+        <li className="group hover:scale-130 transition">
+            <a className=""href={profile} >
                 {children}
             </a>
         </li>
@@ -16,37 +17,33 @@ export function ProfileLink({children, profile}: {children: ReactNode, profile: 
 export default function ProfileLinks({links}: {links: Profile['links']}) {
 
     return (
-        <ul className="inline-full flex flex-row justify-evenly">
+        <ul className="flex flex-row justify-evenly ">
+            <ProfileLink profile={'https://github.com/' + links.github}>
+                <div className="size-13 rounded-full border-primary-300 border-4 sm:border-2 inline-grid text-center place-items-center">
+                    <FaGithub className="fill-white size-6 group-hover:scale-130 group-hover:fill-secondary-300 transition"/>
+                </div>
+                <div className="text-base sm:text-sm text-center opacity-100 sm:opacity-0 group-hover:opacity-100 transition">github</div>
+            </ProfileLink>
+
             <ProfileLink profile={'https://linkedin.com/in/' + links.linkedin}>
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 640" className="fill-white size-10 sm:size-7 ">
-                {/* <!--!Font Awesome Free v7.3.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2026 Fonticons, Inc.--> */}
-                <path d="M196.3 512L103.4 512L103.4 212.9L196.3 212.9L196.3 512zM149.8 172.1C120.1 172.1 96 147.5 96 
-                117.8C96 103.5 101.7 89.9 111.8 79.8C121.9 69.7 135.6 64 149.8 64C164 64 177.7 69.7 187.8 79.8C197.9 89.9 
-                203.6 103.6 203.6 117.8C203.6 147.5 179.5 172.1 149.8 172.1zM543.9 512L451.2 512L451.2 366.4C451.2 331.7 
-                450.5 287.2 402.9 287.2C354.6 287.2 347.2 324.9 347.2 363.9L347.2 512L254.4 512L254.4 212.9L343.5 
-                212.9L343.5 253.7L344.8 253.7C357.2 230.2 387.5 205.4 432.7 205.4C526.7 205.4 544 267.3 544 347.7L544 
-                512L543.9 512z"/></svg>
+                <div className="size-13 rounded-full border-primary-300 border-4 sm:border-2 inline-grid text-center place-items-center">
+                    <FaLinkedinIn className="fill-white size-6 group-hover:scale-130 group-hover:fill-secondary-300 transition"/>
+                </div>
+                <div className="text-base sm:text-sm text-center opacity-100 sm:opacity-0 group-hover:opacity-100 transition">linkedin</div>
             </ProfileLink>
 
             <ProfileLink profile={'mailto:' + links.email}>
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 640" className="fill-white size-10 sm:size-7 ">
-                {/* <!--!Font Awesome Free v7.3.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2026 Fonticons, Inc.--> */}
-                <path d="M125.4 128C91.5 128 64 155.5 64 189.4C64 190.3 64 191.1 64.1 192L64 192L64 448C64 483.3 92.7 
-                512 128 512L512 512C547.3 512 576 483.3 576 448L576 192L575.9 192C575.9 191.1 576 190.3 576 189.4C576 
-                155.5 548.5 128 514.6 128L125.4 128zM528 256.3L528 448C528 456.8 520.8 464 512 464L128 464C119.2 464 112
-                456.8 112 448L112 256.3L266.8 373.7C298.2 397.6 341.7 397.6 373.2 373.7L528 256.3zM112 189.4C112 182 118
-                176 125.4 176L514.6 176C522 176 528 182 528 189.4C528 193.6 526 197.6 522.7 200.1L344.2 335.5C329.9 
-                346.3 310.1 346.3 295.8 335.5L117.3 200.1C114 197.6 112 193.6 112 189.4z"/></svg>
+                <div className="size-13 rounded-full border-primary-300 border-4 sm:border-2 inline-grid text-center place-items-center">
+                    <FaEnvelope className="fill-white size-6 group-hover:scale-130 group-hover:fill-secondary-300 transition"/>
+                </div>
+                <div className="text-base sm:text-sm text-center opacity-100 sm:opacity-0 group-hover:opacity-100 transition">email</div>
             </ProfileLink>
 
             <ProfileLink profile={links.resume}>
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 640" className="fill-white size-10 sm:size-7 ">
-                {/* <!--!Font Awesome Free v7.3.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2026 Fonticons, Inc.--> */}
-                <path d="M304 112L192 112C183.2 112 176 119.2 176 128L176 512C176 520.8 183.2 528 192 528L448 528C456.8
-                528 464 520.8 464 512L464 272L376 272C336.2 272 304 239.8 304 200L304 112zM444.1 224L352 131.9L352
-                200C352 213.3 362.7 224 376 224L444.1 224zM128 128C128 92.7 156.7 64 192 64L325.5 64C342.5 64 358.8 70.7
-                370.8 82.7L493.3 205.3C505.3 217.3 512 233.6 512 250.6L512 512C512 547.3 483.3 576 448 576L192 576C156.7
-                576 128 547.3 128 512L128 128z"/></svg>
+                <div className="size-13 rounded-full border-primary-300 border-4 sm:border-2 inline-grid text-center place-items-center">
+                    <FaFileDownload className="fill-white size-6 group-hover:scale-130 group-hover:fill-secondary-300 transition"/>
+                </div>
+                <div className="text-base sm:text-sm text-center opacity-100 sm:opacity-0 group-hover:opacity-100 transition">resume</div>
             </ProfileLink>
         </ul>
     );
